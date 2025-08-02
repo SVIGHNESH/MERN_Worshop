1a07133574bac3a50fef4029f20ea1a5206787e4
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-02 11:43:20 +0530

    Update Netflix clone: Enhance carousel with additional slides and adjust CSS for layout improvements

---
+++ Day6/marks_Comparison.js
@@ -0,0 +1,22 @@
+let marks = 56;
+if(marks > 80){
+    console.log("You are passed with the grade A ");
+
+}
+else if (marks > 70 ){
+
+    console.log("Your are passed with the Grade B");
+
+}
+
+else if (marks > 50 ){
+
+    console.log("Your are passed with the Grade C");
+    
+}
+
+else if (marks > 35 ){
+
+    console.log("Your are passed!!!!");
+    
+}
---
+++ Day6/script.js
@@ -0,0 +1,48 @@
+// const a = 1 ; 
+
+
+// var b = "where"
+// console.log(b);
+// console.log(age);
+// //hoisting 
+
+// var age = 44
+
+// console.log(age)
+// var age = 40;
+// function plus(a,b){
+//     return a+b;
+// }
+// var sum = plus(3,5);
+
+
+//operator
+//Arithmetic Operator 
+// +,-,*,/,**,%
+
+//Comparision 
+// ==,===,<,>,!=
+
+
+//Assignment Operator
+//=,+=,-=,*=,/=,%=
+
+// let age = 40;
+// let roll_number = "40";
+// console.log(age === roll_number)
+
+
+//Conditionals 
+let age = 60;
+if(age < 18 ){
+    console.log("You Are eligible")
+}
+else if (age < 38){
+    console.log("Yout are super eligible")
+}
+else{
+    console.log("Ypu are super super Eligible ")
+}
+
+
+
