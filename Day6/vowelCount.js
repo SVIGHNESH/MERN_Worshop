328bfda42eb7f5d33efe818b9b9460dedcdcba1b
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-03 14:47:00 +0530

    Refactor vowelCount.js: Comment out unused vowel counting logic and clean up code

--- Day6/vowelCount.js
+++ Day6/vowelCount.js
@@ -1,15 +1,36 @@
-let Vowels = "aeiouAEIOU"
-let Vowcount = 0;
-let ConstCounts = 0;
-let Str = "Vighnesh"
-for(let i = 0 ;  i< Str.length;i++){
-    if( Vowels.includes(Str[i])){
-        Vowcount += 1;
+// let Vowels = "aeiouAEIOU"
+// let Vowcount = 0;
+// let ConstCounts = 0;
+// let Str = "Vighnesh"
+// for(let i = 0 ;  i< Str.length;i++){
+//     if( Vowels.includes(Str[i])){
+//         Vowcount += 1;
+//     }
+//     else{
+//         ConstCounts+=1;
+//     }
+// }
+
+// console.log(Vowcount);
+// console.log(ConstCounts);
+
+let sentence =" Vighnesh is my name of the choice"
+
+//Vighnesh Is My Name Of The Choice 
+
+word = '';
+for(let i = 0 ; i < sentence.length;i++){
+    if(sentence[i] != " "){
+        word += sentence[i];
+        
     }
     else{
-        ConstCounts+=1;
+        word += sentence[i];
+        result += word.charAt(0).toUpperCase() + word.slice(1) + " "
+        word = " "
+
     }
 }
-
-console.log(Vowcount);
-console.log(ConstCounts);
+if(word){
+    result += word.charAt(0).toUpperCase().slice(1)
+}
