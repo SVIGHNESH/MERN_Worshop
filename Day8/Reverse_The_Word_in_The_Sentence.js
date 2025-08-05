e071127230fc8304134d61f9f38b7bc2e507b37c
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-05 13:57:03 +0530

    Refactor Reverse_The_Word_in_The_Sentence.js: Clean up code structure and fix variable name typo

--- Day8/Reverse_The_Word_in_The_Sentence.js
+++ Day8/Reverse_The_Word_in_The_Sentence.js
@@ -1,6 +1,35 @@
 //Reverse the word in a sentence 
 // input -> Hello World
-//output -> World Hello 
+//output -> World Hello
 
-let str = "Hello World" 
-console.log(str.split(" ").reverse().join(" "))
+let senctence = "Hello World";
+
+    let words = [];
+    let word = '';
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] === ' ') {
+            if (word.length > 0) {
+                words.push(word);
+                word = '';
+            }
+        } else {
+            word += sentence[i];
+        }
+    }
+    if (word.length > 0) {
+        words.push(word);
+    }
+
+    let reversed = '';
+    for (let i = words.length - 1; i >= 0; i--) {
+        reversed += words[i];
+        if (i !== 0) {
+            reversed += ' ';    
+        }
+    }
+   console.log(reversed)
+}
+
+ // Output: World Hello 
+
+
