171351ed44c6f5fadf3c67ba83d8ff061850888d
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-05 12:31:26 +0530

    Refactor longest_unique_substring.js: Comment out unused code and clean up logic for finding the longest unique substring

--- Day8/longest_unique_substring.js
+++ Day8/longest_unique_substring.js
@@ -1,37 +1,64 @@
 // longest unique substring
 // input : "abcabcbb" 
-    // let start = 0, maxLen = 0, maxSubstr = "", seen = {};
-    // for (let end = 0; end < s.length; end++) {
-    //     if (seen[s[end]] !== undefined && seen[s[end]] >= start) {
-    //         start = seen[s[end]] + 1;
-    //     }
-    //     seen[s[end]] = end;
-    //     if (end - start + 1 > maxLen) {
-    //         maxLen = end - start + 1;
-    //         maxSubstr = s.substring(start, end + 1);
-    //     }
-    // }
-    // console.log(maxSubstr)
+// let start = 0, maxLen = 0, maxSubstr = "", seen = {};
+// for (let end = 0; end < s.length; end++) {
+//     if (seen[s[end]] !== undefined && seen[s[end]] >= start) {
+//         start = seen[s[end]] + 1;
+//     }
+//     seen[s[end]] = end;
+//     if (end - start + 1 > maxLen) {
+//         maxLen = end - start + 1;
+//         maxSubstr = s.substring(start, end + 1);
+//     }
+// }
+// console.log(maxSubstr)
 
-let s = "abcabcbb"
+// let s = "abcabcbb"
 
-    let maxLen = 0, start = 0, maxSubstr = "";
-    for (let i = 0; i < s.length; i++) {
-        for (let j = start; j < i; j++) {
-            if (s[j] === s[i]) {
-                start = j + 1;
-                break;
-            }
+// let maxLen = 0, start = 0, maxSubstr = "";
+// for (let i = 0; i < s.length; i++) {
+//     for (let j = start; j < i; j++) {
+//         if (s[j] === s[i]) {
+//             start = j + 1;
+//             break;
+//         }
+//     }
+//     if (i - start + 1 > maxLen) {
+//         maxLen = i - start + 1;
+//         maxSubstr = s.substring(start, i + 1);
+//     }
+// }
+// console.log(maxLen);
+// console.log(maxSubstr)
+
+
+
+
+
+
+let w = "abbacbabcd"
+let substring = ""
+let length = 0;
+for (let i = 0; i < w.length; i++) {
+    let sub = " "
+    for (let j = i; j < w.length; j++) {
+        
+        if (sub.includes(w[j])) {
+            break
         }
-        if (i - start + 1 > maxLen) {
-            maxLen = i - start + 1;
-            maxSubstr = s.substring(start, i  + 1 );
+        else {
+            sub += w[j]
         }
     }
-    console.log(maxLen);
-    console.log(maxSubstr)
+        if (length < sub.length) {
+            length = sub.length
+            substring = sub;
+        }
+    
+}
 
-
+console.log(substring)
+console.log(length)
 
 
 
