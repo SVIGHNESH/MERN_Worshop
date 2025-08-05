e042719f6fb7325b163b011f4ec9a9c27d2222aa
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-05 11:58:01 +0530

    Add implementation for finding the longest unique substring in longest_unique_substring.js

---
+++ Day8/longest_unique_substring.js
@@ -0,0 +1,57 @@
+// longest unique substring
+// input : "abcabcbb" 
+    // let start = 0, maxLen = 0, maxSubstr = "", seen = {};
+    // for (let end = 0; end < s.length; end++) {
+    //     if (seen[s[end]] !== undefined && seen[s[end]] >= start) {
+    //         start = seen[s[end]] + 1;
+    //     }
+    //     seen[s[end]] = end;
+    //     if (end - start + 1 > maxLen) {
+    //         maxLen = end - start + 1;
+    //         maxSubstr = s.substring(start, end + 1);
+    //     }
+    // }
+    // console.log(maxSubstr)
+
+let s = "abcabcbb"
+
+    let maxLen = 0, start = 0, maxSubstr = "";
+    for (let i = 0; i < s.length; i++) {
+        for (let j = start; j < i; j++) {
+            if (s[j] === s[i]) {
+                start = j + 1;
+                break;
+            }
+        }
+        if (i - start + 1 > maxLen) {
+            maxLen = i - start + 1;
+            maxSubstr = s.substring(start, i  + 1 );
+        }
+    }
+    console.log(maxLen);
+    console.log(maxSubstr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
