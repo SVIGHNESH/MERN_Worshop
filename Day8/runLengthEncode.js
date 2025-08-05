060abe48177be7681613bdc9204c9206d327728d
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-05 10:18:31 +0530

    Add implementation of run length encoding in runLengthEncode.js

---
+++ Day8/runLengthEncode.js
@@ -0,0 +1,18 @@
+//input : "aaabbcddd"
+let str = "aaabbcdd"
+
+    let result = "";
+    let count = 1;
+    for (let i = 1; i <= str.length; i++) {
+        if (str[i] === str[i - 1]) {
+            count++;
+        } else {
+            result += str[i - 1] + count;
+            count = 1;
+        }
+    }
+    console.log(result)
+
+
+// output : "a3b2c1d3"
+
