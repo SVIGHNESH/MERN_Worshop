339eff43c459b075838decf6615489235831a3df
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-05 14:41:18 +0530

    Refactor Reverse_Vowel_Only.js: Improve code readability by adding braces for clarity in while loops

--- Day8/Reverse_Vowel_Only.js
+++ Day8/Reverse_Vowel_Only.js
@@ -1,19 +1,23 @@
 //Reverse Vowels  Only 
 // input : "hello"
 let s = "hello"
-    const vowels = 'aeiouAEIOU';
-    const arr = s.split('');
-    let left = 0, right = arr.length - 1;
-    while (left < right) {
-        while (left < right && vowels.indexOf(arr[left]) === -1) left++;
-        while (left < right && vowels.indexOf(arr[right]) === -1) right--;
-        if (left < right) {
-            let temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
+const vowels = 'aeiouAEIOU';
+const arr = s.split('');
+let left = 0, right = arr.length - 1;
+while (left < right) {
+    while (left < right && vowels.indexOf(arr[left]) === -1) {
+        left++;
     }
-    console.log(arr.join(''));
+    while (left < right && vowels.indexOf(arr[right]) === -1) {
+        right--;
+    }
+    if (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
+console.log(arr.join(''));
 
