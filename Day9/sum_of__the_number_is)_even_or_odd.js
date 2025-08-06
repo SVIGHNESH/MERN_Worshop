6d35fe05dcdeb2d89528426f676fd1f6832763b2
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 08:08:06 +0000

    Refactor sumEvenOdd function to calculate and log sums of even and odd digits

--- Day9/sum_of__the_number_is)_even_or_odd.js
+++ Day9/sum_of__the_number_is)_even_or_odd.js
@@ -1,11 +1,32 @@
-let number = 343434;
-const isEvenOrOdd = (n) => {
-    let sum = 0;
+// let number = 12345678;
+// const isEvenOrOdd = (n) => {
+//     let sum = 0;
+//     while (n > 0) {
+//         let digit = n % 10;
+//         sum += digit;
+//         n = Math.floor(n / 10);
+//     }
+//     return sum % 2 === 0 ? "Even" : "Odd";
+// }
+// console.log(isEvenOrOdd(number));
+
+
+// find sum of even numbers and odd numbers from given number
+// input: 12345678 => even = 2+4+6+8 ; odd = 1+3+5+7
+
+const sumEvenOdd = (n) => {
+    let evenSum = 0;
+    let oddSum = 0;
     while (n > 0) {
         let digit = n % 10;
-        sum += digit;
+        if (digit % 2 === 0) {
+            evenSum += digit;
+        } else {
+            oddSum += digit;
+        }
         n = Math.floor(n / 10);
     }
-    return sum % 2 === 0 ? "Even" : "Odd";
+   console.log(evenSum)
+   console.log(oddSum)
 }
-console.log(isEvenOrOdd(number));
+sumEvenOdd(123456768) // Output: { evenSum: 20, oddSum: 16 }
