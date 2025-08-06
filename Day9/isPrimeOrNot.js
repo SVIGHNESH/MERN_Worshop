d70712326bceb0f28e8bd642fd0a9665f681ee08
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 08:29:23 +0000

    Optimize isPrime function to reduce iteration by checking up to half of n

--- Day9/isPrimeOrNot.js
+++ Day9/isPrimeOrNot.js
@@ -4,7 +4,7 @@ const isPrime = (n) => {
         {
              return false;
         }
-    for (let i = 2; i < n; i++) {
+    for (let i = 2; i <= Math.floor(n/2); i++) {
         if (n % i === 0)
             {
                 return false;
