9e52493a817e6a71f3401a06d1ac41495e5e7804
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 08:38:31 +0000

    Remove output comments from isPrime function test cases

--- Day9/isPrimeOrNot.js
+++ Day9/isPrimeOrNot.js
@@ -13,13 +13,13 @@ const isPrime = (n) => {
     return true;
 }
 
-console.log(isPrime(5))
-console.log(isPrime(2))
-console.log(isPrime(3))
-console.log(isPrime(4))
-console.log(isPrime(6))
-console.log(isPrime(7))
-console.log(isPrime(8))
+// console.log(isPrime(5))
+// console.log(isPrime(2))
+// console.log(isPrime(3))
+// console.log(isPrime(4))
+// console.log(isPrime(6))
+// console.log(isPrime(7))
+// console.log(isPrime(8))
 console.log(isPrime(9))
 
 
