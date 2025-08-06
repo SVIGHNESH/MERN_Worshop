d21c4257933ca54413776a8a4fed4318d7858045
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 06:37:33 +0000

    Refactor Harshad number check implementation for clarity and encapsulation

--- Day9/IsHarshad_Number.js
+++ Day9/IsHarshad_Number.js
@@ -1,8 +1,22 @@
+// let num = 1234
+
+// let sum = 0;
+// while(num > 0){
+//     sum += num%10;
+//     num = Math.floor(num/10)
+// }
+
+// console.log(sum%num == 0 ? "It is a harshad number" : "It is not a harshad number ")
+
+
 let num = 1234
-let sum = 0;
-while(num > 0){
-    sum += num%10;
-    num = Math.floor(num/10)
+const isHarshad = (a)=>{
+    let sum = 0
+    while(a > 0){
+     sum += a%10;
+     a = Math.floor(a/10)
+ }
+ return sum%num == 0 ? "It is a harshad number" : "It is not a harshad number "
 }
 
-console.log(sum%num == 0 ? "It is a harshad number" : "It is not a harshad number ")
+console.log(isHarshad(num))
