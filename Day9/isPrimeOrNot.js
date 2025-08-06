8f79ed13b92fbc33a2d84eba777ac795d58e6168
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 08:16:22 +0000

    Add isPrime function to determine if a number is prime

---
+++ Day9/isPrimeOrNot.js
@@ -0,0 +1,16 @@
+// find given number is prime or not
+const isPrime = (n) => {
+    if (n <= 1)
+        {
+             return false;
+        }
+    for (let i = 2; i < Math.sqrt(n); i++) {
+        if (n % i === 0)
+            {
+                return false;
+            } 
+    }
+    return true;
+}
+
+
