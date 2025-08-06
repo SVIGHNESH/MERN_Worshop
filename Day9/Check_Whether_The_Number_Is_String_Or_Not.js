d36918e60e26168582c37708b1648dc40c0ab181
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 05:47:16 +0000

    Refactor palindrome check implementation; comment out legacy code and streamline function

--- Day9/Check_Whether_The_Number_Is_String_Or_Not.js
+++ Day9/Check_Whether_The_Number_Is_String_Or_Not.js
@@ -2,11 +2,25 @@
 // input : 1234
 // output : false
 
-let number = 1234
-let rev_num = 0;
-let temp = number;
-while (temp > 0) {
-    rev_num = rev_num * 10 + temp % 10;
-    temp = Math.floor(temp / 10);
+// let number = 1234
+// let rev_num = 0;
+// let temp = number;
+// while (temp > 0) {
+//     rev_num = rev_num * 10 + temp % 10;
+//     temp = Math.floor(temp / 10);
+// }
+// console.log(rev_num === number);
+
+
+const isPalindrome = (a)=>{
+    let rev_num = 0 ;
+    let temp = a;
+    while(temp > 0 ){
+        rev_num = rev_num * 10 + temp % 10;
+        temp = Math.floor(temp / 10);
+    }
+    // console.log(rev_num)
+    return rev_num == a
 }
-console.log(rev_num === number);
+
+console.log(isPalindrome(343))
