c6049814c78e19015b8e616781003da5c49dbf23
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-06 04:40:20 +0000

    Add commented-out hello functions and maintain addition function in function.js

--- Day9/function.js
+++ Day9/function.js
@@ -1,1 +1,15 @@
-console.log("HEELOO")
+// function hello(){
+//     return "Hello"
+// }
+// console.log(hello());
+
+// const hi = ()=>{
+//     return "Hello"
+// }
+// console.log(hi())
+
+
+const addition = () => {
+    return 2+2
+}
+console.log(addition())
