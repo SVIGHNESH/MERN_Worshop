221baf6aa1922f56a6e36c15502b4d1cb4219aa6
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-07 11:54:36 +0530

    Remove commented-out code for the previous implementation of the two sum logic

--- Day10/two_Sum.js
+++ Day10/two_Sum.js
@@ -3,19 +3,19 @@
 let numbers = [1,2,3,6,7,8,9];
 let target = 10;
 let result = [];
-numbers.forEach((element, index) => {
-    let expectedNumber = target - element;
-    if (numbers.includes(expectedNumber) && !result.includes(expectedNumber)) {
-        result.push(element);
-        result.push(expectedNumber);
-    }
-});
-if (result.length > 0) { 
-    console.log(`Two numbers that add up to ${target} are: ${(result[0])} and ${(result[1])}`);
-}
-else {
-    console.log(`No two numbers add up to ${target}`);
-}   
+// numbers.forEach((element, index) => {
+//     let expectedNumber = target - element;
+//     if (numbers.includes(expectedNumber) && !result.includes(expectedNumber)) {
+//         result.push(element);
+//         result.push(expectedNumber);
+//     }
+// });
+// if (result.length > 0) { 
+//     console.log(`Two numbers that add up to ${target} are: ${(result[0])} and ${(result[1])}`);
+// }
+// else {
+//     console.log(`No two numbers add up to ${target}`);
+// }   
 
 
 
