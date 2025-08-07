1b6717057fb7ea18a85bfa3043e0fca771900ff4
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-07 11:29:36 +0530

    Add implementation to find the two missing numbers in an array

---
+++ Day10/find_the_two_missing_numbers.js
@@ -0,0 +1,18 @@
+// find the 2 missing from given array 
+let numbers = [1,2,3,6,7,8,9]
+let missing = []
+numbers.forEach((element, index) => {
+    let expectedNumber = element + 2
+    if(expectedNumber > 10){
+        console.log("Length reached ");
+        return
+    }
+    if (!numbers.includes(expectedNumber)) {
+        missing.push(expectedNumber)
+        console.log(`Missing number is: ${expectedNumber}`);
+    }
+});
+
+console.log(missing);
+// Output: Missing number is: 4
+// Output: Missing number is: 5 
