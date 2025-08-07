e90f4d97d9d16a9d453c98407f51063c92797230
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-07 10:16:55 +0530

    Update number array and comment out reduce and some examples for clarity

--- Day10/array.js
+++ Day10/array.js
@@ -1,5 +1,5 @@
 // Arrays 
-let number = [1,2,3,4,5]
+let number = [10,2,2,4,6]
 // number.forEach((element,index) => {
 //     console.log()
 // });
@@ -8,6 +8,14 @@ let number = [1,2,3,4,5]
 // console.log(changed)
 
 
-// reduce 
-let sum_reduced = number.reduce((c,e)=>c+e)
-console.log(reduced);
+// // reduce 
+// let sum_reduced = number.reduce((c,e)=>c+e)
+// console.log(sum_reduced);
+
+
+// some
+
+// console.log(number.some((n)=>n%2!=0))
+
+
+//every
