891a009954aafb0d542564c113ce22c9b7ba9db9
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-07 11:44:11 +0530

    Update two_Sum.js to change input numbers and target; modify output message to display indices of the numbers

--- Day10/two_Sum.js
+++ Day10/two_Sum.js
@@ -1,7 +1,7 @@
 // two sum 
 // input = 1,2
-let numbers = [2, 7, 11, 15];
-let target = 9;
+let numbers = [1,2,3,6,7,8,9];
+let target = 10;
 let result = [];
 numbers.forEach((element, index) => {
     let expectedNumber = target - element;
@@ -11,7 +11,7 @@ numbers.forEach((element, index) => {
     }
 });
 if (result.length > 0) { 
-    console.log(`Two numbers that add up to ${target} are: ${result[0]} and ${result[1]}`);
+    console.log(`Two numbers that add up to ${target} are: ${result.indexOf(result[0])} and ${result.indexOf(result[1])}`);
 }
 else {
     console.log(`No two numbers add up to ${target}`);
