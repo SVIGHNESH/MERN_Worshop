5defdfcecddd77417ab82d757e009b1bdba994d4
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-08 13:59:48 +0530

    Refactor variable names in anagram check for clarity and consistency

--- Day11/Check_two_Words_are_anagrams_or_not.js
+++ Day11/Check_two_Words_are_anagrams_or_not.js
@@ -1,23 +1,23 @@
 //check two words anagram or not
 
 
-const first = "listen";
-const second = "silent";
+const word1 = "listen";
+const word2 = "silent";
 
-if(first.length !== second.length) {
+if(word1.length !== word2.length) {
     console.log("Not A Anagram.")
 }
 
 const charCount = {};
 
 
-for (let i = 0; i < first.length; i++) {
-    const char = first[i];
+for (let i = 0; i < word1.length; i++) {
+    const char = word1[i];
     charCount[char] = (charCount[char] || 0) + 1;
 }           
 
-for (let i = 0; i < second.length; i++) {
-    const char = second[i];
+for (let i = 0; i < word2.length; i++) {
+    const char = word2[i];
     if (!charCount[char]) {
         console.log("False")
     }
