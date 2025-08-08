9a702253a30bb35f3f9c9b3c445d31b77ecbbc02
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-08 13:41:57 +0530

    Refactor anagram check to use consistent variable names and improve output messages

--- Day11/Check_two_Words_are_anagrams_or_not.js
+++ Day11/Check_two_Words_are_anagrams_or_not.js
@@ -1,29 +1,27 @@
 //check two words anagram or not
 
-let word1 = "Listen"
-let word2 = "Silent"
 
-    const first = "listen";
-    const second = "silent";
-    
-    if (first.length !== second.length) {
-       console.log("False")
+const first = "listen";
+const second = "silent";
+
+if (first.length !== second.length) {
+    console.log("Not A Anagram.")
+}
+
+const charCount = {};
+
+
+for (let i = 0; i < first.length; i++) {
+    const char = first[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+}
+
+for (let i = 0; i < second.length; i++) {
+    const char = second[i];
+    if (!charCount[char]) {
+        console.log("False")
     }
-    
-    const charCount = {};
-    
-    
-    for (let i = 0; i < first.length; i++) {
-        const char = first[i];
-        charCount[char] = (charCount[char] || 0) + 1;
-    }
-    
-    for (let i = 0; i < second.length; i++) {
-        const char = second[i];
-        if (!charCount[char]) {
-            console.log("False")
-        }
-        charCount[char]--;
-    }
-    
-    console.log("True")
+    charCount[char]--;
+}
+
+console.log("True")
