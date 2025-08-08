09bf3f4bd5ef60fe3127a38f7dbfc694b3c28087
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-08 15:19:10 +0530

    Add initial implementation of getValue function and example object structure

---
+++ Day11/make_the_object_more_defined.js
@@ -0,0 +1,18 @@
+// let obj = {name:"Paveen",
+            //     marks:{
+            //         "maths":39,
+            //         "science":40
+            //     },
+            //     "roll_no":22
+            // }
+
+// output ==> {
+//     name:"praveen",
+//     "marks.maths":39,
+//     "marks.science" : 40,
+//     "roll_no" : 22
+// }
+
+
+
+
