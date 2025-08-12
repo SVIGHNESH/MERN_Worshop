ed668ea16bb99d4f6a6df1963b399a3de2b8a9ce
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-12 14:08:06 +0530

    Comment out console log examples for clarity and maintainability

--- Day13/backtick.js
+++ Day13/backtick.js
@@ -1,17 +1,26 @@
-console.log(`
-    this
-    is 
-    the 
-    backtick
-    of th
-    the 
-    JS 
+// console.log(`
+//     this
+//     is 
+//     the 
+//     backtick
+//     of th
+//     the 
+//     JS 
     
     
-    `)
+//     `)
 
 
-    let a = 3
-    let b = 6;
-    console.log(`a value is the ${a}`);
-    console.log(`b value is the ${b}`);
+//     let a = 3
+//     let b = 6;
+//     console.log(`a value is the ${a}`);
+//     console.log(`b value is the ${b}`);
+const fun = (string,{...value})={
+    console.log(string);
+    console.log(value);
+
+}
+
+let hi = "gi hello"
+let num  = 123
+`${fun(hi,{num})}`
