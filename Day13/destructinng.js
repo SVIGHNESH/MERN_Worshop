af903863ba102fabaa1ac3039a80101b0c94df79
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-12 04:58:52 +0000

    Refactor destructuring examples and add a function to log ages

---
+++ Day13/destructinng.js
@@ -0,0 +1,33 @@
+// const arr = [3,2,4]
+
+// const [d, ,f] = arr 
+// console.log(d)
+// console.log(f)
+
+
+// const obj = {
+//     "students":"RBMI",
+//     "Marks":{
+//         "Maths":45,
+//         "English":65,
+//     },
+//     "Location":"UP"
+
+// }
+// const{Marks:{Maths,React}} = obj
+// console.log(Maths)
+// console.log(React)
+
+
+// const obj = {
+//     "students":"RBMI",
+//     "Marks":[3,42,4,2,4,2]
+//     "Location":"UP"
+
+// }
+// const{Maths:[akgfm,afkjahf]}
+
+const fun = (...ages)=>{
+    console.log(ages)
+}
+fun({"age":45,"name":"VIghneh"})
