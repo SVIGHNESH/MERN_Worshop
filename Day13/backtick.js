731d86bd2e31185e47fcd45b78813af4c94e832c
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-12 14:01:16 +0530

    Add initial console log output for backtick demonstration

---
+++ Day13/backtick.js
@@ -0,0 +1,17 @@
+console.log(`
+    this
+    is 
+    the 
+    backtick
+    of th
+    the 
+    JS 
+    
+    
+    `)
+
+
+    let a = 3
+    let b = 6;
+    console.log(`a value is the ${a}`);
+    console.log(`b value is the ${b}`);
