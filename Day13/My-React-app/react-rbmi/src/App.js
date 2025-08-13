a60c93f3e791d3c12d80ae636f0353bf7353c1bc
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-13 10:09:08 +0530

    asgertg

--- Day13/My-React-app/react-rbmi/src/App.js
+++ Day13/My-React-app/react-rbmi/src/App.js
@@ -8,12 +8,16 @@ function App() {
     "color": "blue",
     "background-color" : "yellow"
   }
+  const cssOfH3 = {
+    "border" :"solid 1px black",
+    "background-color":"blue"
+  }
   return (
     <div className="App">
 
       <h1  style={styles}>Hi is True</h1>
       <h1 style = {{"color" : "red"}}>Hi is False</h1>
-      
+      <h3 style ={cssOfH3}>This is the one of the man of the america</h3>
   
     </div>
   
