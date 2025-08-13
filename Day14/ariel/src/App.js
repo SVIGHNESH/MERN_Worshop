08a56bf5c50cd35ee6a86792b80dd3174790c49c
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-13 11:34:57 +0530

    qwerty

--- Day14/ariel/src/App.js
+++ Day14/ariel/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import Main from './Main.jsx';
+import Footer from './Footer.jsx'
 function App() {
   return(
     <div>
       <Navbar/>
+      <br/>
       <Main /> 
+      <Footer /> 
     </div>
   );
 }
--- Day14/ariel/src/Footer.jsx
+++ Day14/ariel/src/Footer.jsx
@@ -1,9 +1,24 @@
 import './Footer.css'
 function Footer(){
     return(
-        <>
-        
-        </>
+        <div className='FooterSection'>
+            <div className='firstPartOfFooterSection'>
+                <p>Follow Us</p>
+                <div>
+                    <a href="https://linkedin.com"> linkedin</a>
+                    <a href="https://instagram.com"> Instagram</a>
+                    <a href="https://x.com"> X</a>
+                </div>
+            </div>
+            <div className='secondPartOfFooterSection'>
+                <div>P&G Family</div>
+                <div>Terms & Condition</div>
+                <div>Privacy</div>
+                <div>Ad Choices </div>
+                <div>Sitemap</div>
+                <div>Your Privacy Choices</div>
+            </div>
+        </div>
     );
 }
 export default Footer
