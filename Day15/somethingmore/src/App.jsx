ed2cccdbcdf42209639f8bde10e0a005346752b3
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-14 10:42:08 +0530

    Created the counter app

--- Day15/somethingmore/src/App.jsx
+++ Day15/somethingmore/src/App.jsx
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let[data,setData]=useState(0)
+  const decreasing = ()=>{
+    
+    setData(data-1)
+    
+    
+  }
+  const increasing = ()=>{
+    
+    setData(data+1)
+    
+    
+  }
+ 
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div>{data}</div>
+      <br />
+      <br />
+      <button onClick = {decreasing}>Decrease by 1  </button>
+      <button onClick = {increasing}>Increase by 1  </button>
+
+    </>
   );
 }
 
