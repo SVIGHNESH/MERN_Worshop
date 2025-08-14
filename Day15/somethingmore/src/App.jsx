d77b3e0406e9a3e6f69d4d7a6b6051fc3a1fd031
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-14 10:56:22 +0530

    asdfghj

--- Day15/somethingmore/src/App.jsx
+++ Day15/somethingmore/src/App.jsx
@@ -3,47 +3,50 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let[data,setData]=useState(0)
-  const decreasing = ()=>{
-    
+  let [data, setData] = useState(0)
+  const decreasing = () => {
+
     // setData(data-1)
-    
+
     // setData(data-1)
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    setData((prev => prev -1 ))
-    
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+    // setData((prev => prev - 1))
+
+    setData(data-10)
+
   }
-  const increasing = ()=>{
-    
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    setData((next)=>next+1)
-    
-    
+  const increasing = () => {
+
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+    // setData((next)=>next+1)
+
+    setData(data+ 10)
+
+
   }
- 
+
   return (
     <>
       <div>{data}</div>
       <br />
       <br />
-      <button onClick = {decreasing}>Decrease by 10  </button>
-      <button onClick = {increasing}>Increase by 10  </button>
+      <button onClick={decreasing}>Decrease by 10  </button>
+      <button onClick={increasing}>Increase by 10  </button>
 
     </>
   );
