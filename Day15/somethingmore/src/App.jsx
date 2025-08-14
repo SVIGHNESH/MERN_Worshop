d795dc06d3195e772200d763f42efd76ce90e78f
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-14 12:08:46 +0530

    DOMNE WRLKJFFHAKLJFHLJSKF:SK

--- Day15/somethingmore/src/App.jsx
+++ Day15/somethingmore/src/App.jsx
@@ -2,10 +2,10 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [inc, setInc] = useState(0)
-  let [dec, setDec] = useState(0)
-  let [data, setData] = useState("")
-  const decreasing = () => {
+  // let [inc, setInc] = useState(0)
+  // let [dec, setDec] = useState(0)
+  let [data, setData] = useState("Normal")
+  // const decreasing = () => {
 
     // setData(data-1)
 
@@ -22,10 +22,10 @@ function App() {
     // setData((prev => prev - 1))
     // setDec((prev)=>prev -1)
     // setData(`Decrease by ${dec - 1 }`)
-     setDec(dec - 1)
-    setData(`Decrease by ${dec-1}`)
-  }
-  const increasing = () => {
+  //    setDec(dec - 1)
+  //   setData(`Decrease by ${dec-1}`)
+  // }
+  // const increasing = () => {
 
     // setData((next)=>next+1)
     // setData((next)=>next+1)
@@ -38,12 +38,18 @@ function App() {
     // setData((next)=>next+1)
 
       //  setData(`Increse  by ${inc  + 1 }`)
-    setInc(inc + 1)
-    setData(`Increase by ${inc+1 }`)
+  //   setInc(inc + 1)
+  //   setData(`Increase by ${inc+1 }`)
 
 
+  // }
+
+
+  const changingInTheInput = (e)=>{
+    setData(e.target.value)
   }
 
+  
   return (
     <>
 
@@ -51,11 +57,16 @@ function App() {
       <div>{data} </div>
       {/* <div>{dec} </div> */}
       {/* <div>{dec} </div> */}
-      <br />
-      <br />
-      <button onClick={decreasing}>Decrease by 1  </button>
-      <button onClick={increasing}>Increase by 1  </button>
-
+      {/* <br /> */}
+      
+      {/* <button onClick={decreasing}>Decrease by 1  </button> */}
+      {/* <button onClick={increasing}>Increase by 1  </button> */}
+      {/* <inpu */}
+      <input
+      
+      value={data}
+      onChange={changingInTheInput}
+      />
     </>
   );
 }
