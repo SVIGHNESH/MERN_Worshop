6beaeb97ccfe193e09a084890e353cd9dd957175
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-14 13:51:36 +0530

    lfj;lsakhfjasklfhsdlkjfhsdfk

--- Day15/somethingmore/src/App.jsx
+++ Day15/somethingmore/src/App.jsx
@@ -4,40 +4,46 @@ import { useState } from 'react';
 function App() {
   // let [inc, setInc] = useState(0)
   // let [dec, setDec] = useState(0)
-  let [data, setData] = useState("Normal")
+  let [d1, setD1] = useState({ "email": "gjkhgkj", "name": "jhfjg" })
+  let [email, setEmail] = useState("")
+  let [name, setName] = useState("")
+  // const{email,name} = data;
+
+  // let [d2, setD2] = useState("Normal")
+  console.log(email);
   // const decreasing = () => {
 
-    // setData(data-1)
+  // setData(data-1)
 
-    // setData(data-1)
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setData((prev => prev - 1))
-    // setDec((prev)=>prev -1)
-    // setData(`Decrease by ${dec - 1 }`)
+  // setData(data-1)
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setData((prev => prev - 1))
+  // setDec((prev)=>prev -1)
+  // setData(`Decrease by ${dec - 1 }`)
   //    setDec(dec - 1)
   //   setData(`Decrease by ${dec-1}`)
   // }
   // const increasing = () => {
 
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
-    // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
+  // setData((next)=>next+1)
 
-      //  setData(`Increse  by ${inc  + 1 }`)
+  //  setData(`Increse  by ${inc  + 1 }`)
   //   setInc(inc + 1)
   //   setData(`Increase by ${inc+1 }`)
 
@@ -45,28 +51,67 @@ function App() {
   // }
 
 
-  const changingInTheInput = (e)=>{
-    setData(e.target.value)
+  // const changingInTheInput = (e)=>{
+  //   setData(e.target.value)
+  // }
+
+
+  const emailChange = (e) => {
+    // const {email,name} = e.target
+    // setData({...data,
+    //         [email]:value
+    //     })
+
+    setEmail(e.target.value)
+
+  }
+  const nameChange = (e) => {
+    setName(e.target.value)
+  } 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert([name, email])
   }
 
-  
   return (
     <>
 
       {/* <div>{inc} </div> */}
-      <div>{data} </div>
+      {/* <div>{data} </div> */}
       {/* <div>{dec} </div> */}
       {/* <div>{dec} </div> */}
       {/* <br /> */}
-      
+
       {/* <button onClick={decreasing}>Decrease by 1  </button> */}
       {/* <button onClick={increasing}>Increase by 1  </button> */}
       {/* <inpu */}
-      <input
+      {/* <input
       
       value={data}
       onChange={changingInTheInput}
       />
+      <input
+      
+      value={data}
+      onChange={changingInTheInput}
+      /> */}
+
+
+
+      <form onSubmit={handleSubmit}>
+        <input type="email"
+          value={email}
+          onChange={emailChange}
+        />
+
+        <input type="text"
+          value={name}
+          onChange={nameChange}
+        />
+
+        <input type='submit' />
+      </form>
     </>
   );
 }
