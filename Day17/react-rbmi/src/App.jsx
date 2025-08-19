db4c9fde26a5657b835bd247546a1afa98f8ee4a
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-19 12:19:40 +0530

    fix: update API endpoint and improve data fetching logic in App component

--- Day17/react-rbmi/src/App.jsx
+++ Day17/react-rbmi/src/App.jsx
@@ -6,25 +6,32 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const[data,setData] = useState()
-  let api = "http://localhost:5000/0"
-  const getData = (id)=>{
-    return  axios.get(`${api}/${id}`)
-
-  }
-  const get =async(id)=>{
-      let data = await getData(id)
-      setData(data)
-      console.log(data)
-  }
-  // useEffect(async()=>{
-  //    get()
-  // })
-  return (
+  const [data, setData] = useState(null);
+  let api = "http://localhost:5000";
+  const getData = (id) => {
+    return axios.get(`${api}/${id}`);
+  };
+  
+ 
+  const get = async (id) => {
+    
+      let data = await getData(id);
+      setData(data);
+      console.log(data);
+    
+  };
+  return(
     <>
-        <button onClick={()=>get(0)}>
-          Get Data Person 2 
+        <button onClick={() => get(0)}>
+          Get Data Person 0
         </button>
+        <button onClick={() => get(1)}>
+          Get Data Person 1
+        </button>
+        <button onClick={() => get(2)}>
+          Get Data Person 2
+        </button>
+        
     </>
   )
 }
