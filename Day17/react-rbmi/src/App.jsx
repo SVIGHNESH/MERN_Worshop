8ff6a676b7e093141ec65554ddd7a1eb0d6a858c
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-19 11:13:43 +0530

    feat: add axios dependency and update App component to fetch data from API

--- Day17/react-rbmi/src/App.jsx
+++ Day17/react-rbmi/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import React from 'react'
+import {axios} from "axios"
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-
+  let api = "http://localhost:5000/0"
+    const data = axios.get(api)
+    console.log(data)
   return (
     <>
       
