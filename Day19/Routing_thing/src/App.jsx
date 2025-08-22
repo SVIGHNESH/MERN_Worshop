5f417c25547ee191b98f729fda0a3742b8ef925f
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-22 11:39:18 +0530

    feat: add Bootstrap and React-Bootstrap dependencies, remove unused App.css styles

--- Day19/Routing_thing/src/App.jsx
+++ Day19/Routing_thing/src/App.jsx
@@ -1,34 +1,55 @@
-import {useEffect, useState} from 'react';
+// import {useEffect, useState} from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+// import Navbar from './Navbar
 // import Home from './Home'
 
 function App(){
-  const [login,setLogin] = useState(false);
-  const [dark,setDark] = useState(false);
-  useEffect(()=>{
-      setLogin(localStorage.getItem("login"))
-      console.log(login,"from useEffect");
-  })
+  // const [login,setLogin] = useState(false);
+  // const [dark,setDark] = useState(false);
+  // useEffect(()=>{
+  //     setLogin(localStorage.getItem("login"))
+  //     console.log(login,"from useEffect");
+  // })
   
   
-  const loogin =()=>{
-    localStorage.setItem("login",true);
+  // const loogin =()=>{
+  //   localStorage.setItem("login",true);
    
-    setLogin(localStorage.getItem("login"))
-          console.log(login,"Inside The Login Function ");
+  //   setLogin(localStorage.getItem("login"))
+  //         console.log(login,"Inside The Login Function ");
 
-  }
+  // }
 
-  const loogout =()=>{
-      localStorage.getItem("login",false);
-      setLogin(localStorage.getItem("login"))
-     console.log(login,"Inside the Logout Function ");
-  }
+  // const loogout =()=>{
+  //     localStorage.getItem("login",false);
+  //     setLogin(localStorage.getItem("login"))
+  //    console.log(login,"Inside the Logout Function ");
+  // }
   return(
+    // <div>
+    //   <button style={{background: dark?"black":"blue"}} onClick={loogin}>Login</button>
+    //   <button onClick={loogout}>Logout</button>
+    //   {/* <Home/> */}
+    // </div>
+
+
     <div>
-      <button style={{background: dark?"black":"blue"}} onClick={loogin}>Login</button>
-      <button onClick={loogout}>Logout</button>
-      {/* <Home/> */}
+      <div className='container-fluid'>
+           {/* <p className='bg-danger'>THis is the nAv Bar</p> */}
+           <div className='row'>
+
+           <div className='col-12 col-md-4 col-lg-6 bg-danger' >1</div>
+
+           <div className='col-12 col-md-4 col-lg-6 bg-primary'>2</div>
+           
+           <div className='col-12 col-md-4 col-lg-12 bg-success' >3</div>
+          
+           {/* <div>5</div>
+           <div>6</div> */}
+           </div>
+        </div>
     </div>
   );
 }
