44c7ecddaa9002053aba241e3da704910391c3d1
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-25 14:18:25 +0530

    feat: update package.json with author name and module type; enhance server.js for user creation and JSON parsing

--- Day20/backend/server.js
+++ Day20/backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 
 let app = express()
 app.use(cors())
+app.use(express.json())
 
 
 let data = [
@@ -24,7 +25,13 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/:id",(req,res)=>{
+app.post('/',(req,res)=>{
+    let id = data[data.length -1].id
+
+    let new_user={"id":id+1, ...req.body}
+    data = [...data,new_user]
+    res.send(data)
+})
 app.get("/:id", (req, res) => {
     let { id } = req.params;
     const user = data.find(item => item.id === parseInt(id));
@@ -32,9 +39,9 @@ app.get("/:id", (req, res) => {
         res.json(user);
     } else {
         res.status(404).json({ error: "User not found" });
+    }
+});
+
 app.listen(5000, () => {
     console.log("working from 5000");
-});
-app.listen(5000,()=>{
-    console.log("working from   5000 ")
-})
+});  
