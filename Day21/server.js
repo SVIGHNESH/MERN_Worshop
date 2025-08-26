9f518d98c2ebb36e9669b0e38fe69b195a99f767
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-08-26 11:19:57 +0530

    feat: add POST endpoint for creating new users in server.js

--- Day21/server.js
+++ Day21/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 let app = express();
+app.use(express.json());
 
 let data = [{
     "id":1,
@@ -29,6 +30,17 @@ app.get("/:id",(req,res)=>{
     })
     res.send(user);
 })
+
+
+
+app.post("/", (req, res) => {
+  let newUser = req.body;
+  let id = data[data.length - 1].id + 1;
+ newUser = {"id":id+1,...newUser}
+  data.push(newUser);
+  res.send(data);
+});
+
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
